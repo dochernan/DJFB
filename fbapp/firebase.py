d5d5f7e63e6384bc8ca549b34b4c1e56599f6131
dochernan <39423616+dochernan@users.noreply.github.com>
2025-02-17 15:02:39 +0800

    Add files via upload

--- fbapp/firebase.py
+++ fbapp/firebase.py
@@ -6,13 +6,36 @@ import os
 
 # Path to the serviceAccountKey.json file
 current_directory = os.path.dirname(os.path.abspath(__file__))
-json_path = os.path.join(current_directory, 'gothic-context-831-firebase-adminsdk-fbsvc-5c2fe4b90f.json')
+json_path = os.path.join(current_directory, 'gothic-context-831-firebase-adminsdk-fbsvc-a78a426122.json')
+
+import json
+import base64
+
+firebase_db_url = 'https://gothic-context-831-default-rtdb.asia-southeast1.firebasedatabase.app/'
+
+if not firebase_admin._apps:
+    encoded_creds = os.getenv('FIREBASE_CREDENTIALS')
+
+    if encoded_creds:
+        # Decode base64 string back to JSON
+        decoded_creds = base64.b64decode(encoded_creds).decode('utf-8')
+        cred_dict = json.loads(decoded_creds)
+        cred = credentials.Certificate(cred_dict)
+    else:
+        raise ValueError("FIREBASE_CREDENTIALS environment variable is not set")
+
+    firebase_admin.initialize_app(cred, {'databaseURL': firebase_db_url})
+
+
 
 # Initialize the Firebase Admin SDK
 cred = credentials.Certificate(json_path)
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://gothic-context-831-default-rtdb.asia-southeast1.firebasedatabase.app/'  # Replace with your Firebase database URL
 })
+
+
+
 
 # Reference to the Realtime Database
 database_ref = db.reference()
